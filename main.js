3fd875e9d881a425e28a0fb8850555ce569558c4
Jessen Gilbert <33400381+Roost312@users.noreply.github.com>
2023-06-24 12:18:48 -0400

    feat: render shapes in dom (#1)
    * chore: add link to styles and js
    
    * chore: add main element and array
    
    * feat: render shapes on dom
    
    * fixup! feat: render shapes on dom

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
 // JavaScript
+
+const colors = [
+    {
+        color: 'red',
+        size: '100%',
+    },
+    {
+        color: 'purple',
+        size: '80%',
+    },
+    {
+        color: 'blue',
+        size: '60%',
+    },
+    {
+        color: 'yellow',
+        size: '40%',
+    },
+    {
+        color: 'orange',
+        size: '20%',
+    },
+    {
+        color: 'pink',
+        size: '10%',
+    }
+];
+
+const squaresDiv = document.getElementById('squares');
+
+for (let i = 0; i < 6; i++) {
+    const newSpan = document.createElement('div');
+    console.log(newSpan)
+    newSpan.style.backgroundColor = colors[i].color;
+    newSpan.style.width = colors[i].size;
+    newSpan.style.height = '100px';
+    squaresDiv.appendChild(newSpan);
+}
