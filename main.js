01ff7bf58283f0176cdabe914357a6dc67536654
Jessen Gilbert <jessen.gilbert@yahoo.com>
2023-06-24 11:52:52 -0400

    chore: add main element and array

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 // JavaScript
+
+const colors = [
+    {
+        color: 'red',
+        size: '100%',
+    },
+    {
+        color: 'purple',
+        size: '80%',
+    },
+    {
+        color: 'blue',
+        size: '60%',
+    },
+    {
+        color: 'yellow',
+        size: '40%',
+    },
+    {
+        color: 'orange',
+        size: '20%',
+    },
+    {
+        color: 'pink',
+        size: '10%',
+    }
+];
